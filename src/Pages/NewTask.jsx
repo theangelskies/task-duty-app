be418a66eebc9ae267a47201c50e8997f5efe24a
AngelSkies <theangelskies13@yahoo.com>
2025-09-26 09:41:15 +0200

    new task edit

--- src/Pages/NewTask.jsx
+++ src/Pages/NewTask.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Form from '../components/form';
+import Form from '../components/Form';
 
 const NewTask = () => {
   const [formData, setFormData] = useState({
