1a5ece6ce08d66de96c39493a800b50b63aa93ae
AngelSkies <theangelskies13@yahoo.com>
2025-09-26 09:47:01 +0200

    form edit

--- src/Pages/Task.jsx
+++ src/Pages/Task.jsx
@@ -1,6 +1,6 @@
 import React, {useState,useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import Form from '../components/form';
+import Form from '../components/Form';
 import { taskData } from '../db';
 
 const Task = () => {
